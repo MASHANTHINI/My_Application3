544008dd648a6dd2118bffb150ea22721c6fea5a
MASHANTHINI <mashanthinib2@gmail.com>
2025-09-17 19:07:38 +0530

    saved

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.btnLogin);
-        btnSignup = findViewById(R.id.btnSignup);
 
         // Go to Login Page
         btnLogin.setOnClickListener(v ->
